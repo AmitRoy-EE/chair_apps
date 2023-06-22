81842190d159342c381ac811ce7f280cc476ee90
David Huckebrink <david.huckebrink@ruhr-uni-bochum.de>
2023-06-22 16:13:57 +0200

    change "app.py" names

--- pages/1_Household_ESM🏠⚡🪫.py
+++ pages/1_Household_ESM🏠⚡🪫.py
@@ -4,9 +4,9 @@ submodule_paths = [entry for entry in sys.path if "submodules/" in entry]
 for path in submodule_paths:
     sys.path.remove(path)
 
-
 sys.path.insert(0,"submodules/oemof_household")
 
-from app import run
+
+from oemof_app import run
 
 run()
