ed63feea9459f2bbf79d75c37683229c1ea41f7b
AmitRoy-EE <amitmeruet@gmail.com>
2026-05-04 15:04:32 +0200

    Refactor sys.path for oemof_household import

--- pages/1_Household_ESM🏠⚡🪫.py
+++ pages/1_Household_ESM🏠⚡🪫.py
@@ -1,7 +1,10 @@
 import sys
 from pathlib import Path
 
-sys.path.append(str(Path(__file__).resolve().parents[1] / "submodules" / "oemof_household"))
+ROOT_DIR = Path(__file__).resolve().parents[1]
+OEMOF_DIR = ROOT_DIR / "submodules" / "oemof_household"
+
+sys.path.insert(0, str(OEMOF_DIR))
 
 from oemof_app import run
 
